705c2d55bce672ce92193d5171053ce327bd3b3b
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-07-05 12:35:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 let  button=document.getElementById("enterBtn");
 
-button.addEventListener("click",()=>
+button.addEventListener("click",()=>{
 	let para=document.getElementById("status");
 	let head=document.createElement("h1");
     head.innerText="Entered metaverse";
 	para.replaceWith(head);
-    
+}
 )
